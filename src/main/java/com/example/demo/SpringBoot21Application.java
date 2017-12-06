bb91c9fc6cfbcc6ea897a74dde76e62120f0b705
Tory Lamkin <torylamkin@Torys-MacBook-Air.local>
2017-12-06 16:31:08 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot21Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot21Application.class, args);
+	}
+}
